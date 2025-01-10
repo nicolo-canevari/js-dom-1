2e977e96abd0cfb2ec7b28d1b3b6e3b9d8d3df67
nicolo_canevari <nico.canevari@outlook.it>
2025-01-10 16:12:30 +0100

    [feat] funzione pere accendere/spegnere la lampadina, cambiare testo e sfondo di input

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -7,4 +7,39 @@ document.addEventListener('DOMContentLoaded', function () {
     // Variabile Booleana "Accesa o spenta"
     let luceAccesaStatus = false;
 
-})
+
+    // funzione per azionare il bottone e cambiare scritta
+    bottone.addEventListener('click', function () {
+
+        if (luceAccesaStatus) {
+
+            // Se la luce è accesa, spegnila
+            luceSpenta.style.display = 'block';
+            luceAccesa.style.display = 'none';
+
+            // Cambia il testo del bottone
+            bottone.value = 'Accendi';
+
+            // Rimuove la classe "spegni" , cambia lo sfondo
+            bottone.classList.remove('spegni');
+
+        } else {
+
+            // Se la luce è spenta, accendila
+            luceSpenta.style.display = 'none';
+            luceAccesa.style.display = 'block';
+
+            // Cambia il testo del bottone
+            bottone.value = 'Spegni';
+
+            // Aggiungi la classe "spegni" , cambia lo sfondo
+            bottone.classList.add('spegni');
+        }
+
+        // Alterna lo stato della luce
+        luceAccesaStatus = !luceAccesaStatus;
+
+    });
+
+
+});
