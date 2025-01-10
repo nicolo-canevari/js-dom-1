01ba9a7dc595be895b599164cb1c44cbf1d6988c
nicolo_canevari <nico.canevari@outlook.it>
2025-01-10 16:05:27 +0100

    [feat] first part js

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,10 @@
+// variabili per bottone e i due 'stati' della lampadina
+document.addEventListener('DOMContentLoaded', function () {
+    const bottone = document.getElementById('accendiBtn');
+    const luceSpenta = document.getElementById('luce_spenta');
+    const luceAccesa = document.getElementById('luce_accesa');
+
+    // Variabile Booleana "Accesa o spenta"
+    let luceAccesaStatus = false;
+
+})
